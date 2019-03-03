34706fa157d089d907aadd58297deb1dc017df05
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-03-03 15:43:51 -0300

    Adicionando um botao e administrando State

--- App.js
+++ App.js
@@ -13,25 +13,46 @@ import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native'
 export default class App extends Component {
 
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangedHandler = (val) => {
     this.setState({placeName: val})
   }
 
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === ""){ //verifica se está vazio
+      return;
+    }
+
+    this.setState(prevState => {
+      return { //coloca o nome do input text na lista de lugares
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+
   render() {
+    //o map percorre meu array e renderiza cada elemento no Text, 
+    //ao chamar a constante tenho o elemento renderizado no Text
+    const placesOutput = this.state.places.map((place, i) => <Text key={i}>{place}</Text>)
     return (
       <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput 
         style={styles.placeInput}
-        placeholder= "Hello"
+        placeholder= "An awesome place"
         value={this.state.placeName}
         onChangeText={this.placeNameChangedHandler} />
-        <Button style={styles.placeButton} title="Add"/>
+        <Button 
+        style={styles.placeButton} 
+        title="Add"
+        onPress={this.placeSubmitHandler}/>
       </View>
-       
+        <View>
+          {placesOutput}
+        </View>       
       </View>
     );
   }
